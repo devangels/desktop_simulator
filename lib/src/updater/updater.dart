561a551ac69967669855b1306e8637a6deed8b06
Norbert <norbertkozsir@gmail.com>
2018-12-11 14:08:11 +0100

    Finished v0.1 of updater

--- lib/src/updater/updater.dart
+++ lib/src/updater/updater.dart
@@ -1,14 +1,19 @@
 import 'dart:io';
 import 'package:archive/archive.dart';
+import 'package:path/path.dart' as p;
 
 void main(List<String> arguments) {
-  Updater u = Updater();
+  Updater u = Updater(true);
   u.tryUpdating();
 }
 
 class Updater {
+  Updater(this.verbose);
+
 
   String get ps => Platform.pathSeparator;
+
+  final bool verbose;
 
   void tryUpdating() async {
     if(_needsUpdate()) {
@@ -16,27 +21,49 @@ class Updater {
     }
   }
 
+  printV(String it) {
+    if(verbose) {
+      print(it);
+    }
+  }
 
+  // TODO take a look at how to tools handle verbosity
   void _update() async {
-    // Search for Flutter
+
+    printV("Seaching for Flutter...");
     ProcessResult results = await Process.run('where flutter', []);
 
     String res = results.stdout;
+    if(res == null ||res.isEmpty) {
+      throw Exception("Flutter could not be found in path\n"
+          "Check to see if where flutter returns a vailid result. \n"
+          "If you are on windows, simply add Flutter to your path");
+    }
     String firstLine = res.split("\n")[0];
-
-    print("Ok $firstLine");
-    List<String> it = firstLine.split("$ps");
+    List<String> it = p.context.split(firstLine);
     String directory = it.getRange(0, it.length - 1).join("$ps");
+    printV("Found Flutter at $directory");
 
+    printV("Seaching for engine hash...");
     String engineHash = await _getEngineHash(directory);
-    print("Found hash: $engineHash");
+    printV("Found engine hash: $engineHash");
 
     File icuDat = _getICUDatFile(directory);
 
-    icuDat.copy("F:/flutter-simulator-dart/desktop_simulator${ps}icudtl.dat");
+
+    String destinationDirectory = File(Platform.script.toString()).parent.parent.parent.parent.path;
+    if(destinationDirectory.startsWith("file:///")) {
+      destinationDirectory = destinationDirectory.substring("file:///".length);
+    }
+
+    printV("Copying ${icuDat.path} to $destinationDirectory ...");
+
+    await icuDat.copy("$destinationDirectory${ps}icudtl.dat");
 
     File engineZip = await _downloadEngine(engineHash);
     File extractedEngine = _extractEngine(engineZip);
+    printV("Finished extracting the engine, cleaning up ...");
+    await engineZip.delete();
   }
 
 
@@ -48,17 +75,34 @@ class Updater {
   Future<String> _getEngineHash(String flutterPath) async {
     String pathToEngineHash = "$flutterPath${ps}cache${ps}engine.stamp";
     File engineFile = File(pathToEngineHash);
+    if(!engineFile.existsSync()) {
+      throw Exception("Could not find engine.stamp at the following location: \n"
+          "$pathToEngineHash .\n"
+          "This file contains the Flutter engine version which it is compiled against \n"
+          "if you can't find the file there something with your Flutter installation might be wrong \n"
+          "run 'flutter doctor'");
+    }
     return await engineFile.readAsString();
   }
 
 
   //TODO this is window only atm
   File _getICUDatFile(String flutterPath) {
-    return File("$flutterPath${ps}cache${ps}artifacts${ps}engine${ps}windows-x64${ps}icudtl.dat");
+    String pathToIcu = "$flutterPath${ps}cache${ps}artifacts${ps}engine${ps}windows-x64${ps}icudtl.dat";
+    File file =  File(pathToIcu);
+    if(!file.existsSync()) {
+      throw Exception("icudtl.dat could not be found at location: \n"
+          "$pathToIcu . \n"
+          "please check if the file is there. If it's not this might indicate something is wrong \n"
+          "with your Flutter installation. Run 'flutter doctor'");
+    }
+    return file;
   }
 
 
   File _extractEngine(File zipFile) {
+    printV("Extracting the flutter_engine.dll ...");
+
     // Read the Zip file from disk.
     List<int> bytes = zipFile.readAsBytesSync();
 
@@ -69,8 +113,9 @@ class Updater {
     for (ArchiveFile file in archive) {
       String filename = file.name;
       if (file.isFile && filename == "flutter_engine.dll") {
+        printV("Found flutter_engine.dll in archive");
         List<int> data = file.content;
-        return File('out/' + filename)
+        return File(filename)
           ..createSync(recursive: true)
           ..writeAsBytesSync(data);
         // Found the file
@@ -85,10 +130,12 @@ class Updater {
   /// Return the handle to a compressed engine .zip file
   Future<File> _downloadEngine(String engineHash) async {
     File embedder = File('windows-x64-embedder.zip');
-    HttpClientRequest request = await HttpClient().getUrl(Uri.parse('https://storage.googleapis.com/flutter_infra/flutter/$engineHash/windows-x64/windows-x64-embedder.zip'));
+    String url = 'https://storage.googleapis.com/flutter_infra/flutter/$engineHash/windows-x64/windows-x64-embedder.zip';
+    printV("Downloading $url ...");
+    HttpClientRequest request = await HttpClient().getUrl(Uri.parse(url));
     HttpClientResponse response = await request.close();
     int contentLength = response.contentLength;
-    print("Need to download $contentLength bytes");
+    printV("Need to download $contentLength bytes");
 
     int currentBytes = 0;
     int currentPercentage = 0;
@@ -97,13 +144,13 @@ class Updater {
       currentBytes += it.length;
       int newPercentage = ((currentBytes / contentLength) * 100).floor();
       if(currentPercentage != newPercentage) {
-        print("At $newPercentage %");
+        printV("At $newPercentage %");
       }
       currentPercentage = newPercentage;
 
       return it;
     }).pipe(embedder.openWrite());
-    print("finished downloading");
+    printV("Finished downloading the engine zip");
     return embedder;
   }
 
