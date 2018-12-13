d0177277c28e633f005f5eda94279f5ff104e63a
Norbert <norbertkozsir@gmail.com>
2018-12-13 19:20:35 +0100

    added width and height arguments

--- bin/main.dart
+++ bin/main.dart
@@ -1,28 +1,52 @@
 import 'package:desktop_simulator/desktop_simulator.dart';
 import 'package:desktop_simulator/src/simulator_window.dart';
+import 'package:args/args.dart';
 
 
 void main(List<String> arguments) {
 
   // First arg is asset path
   // the rest can be forwarded to commandLineArgs
+  
+  ArgParser parser = ArgParser();
 
-  print("Started with args: $arguments");
-  String assetPath;
-  if(arguments.length > 0) {
-    assetPath = arguments[0];
-  } else {
-    print("Could not find Flutter assets");
+  parser.addOption("width", abbr: "w", help: "The width of this window in pixels",
+    valueHelp: "WIDTH", defaultsTo: "400");
+  parser.addOption("height", abbr: "h", help: "The height of this window in pixels",
+    valueHelp: "HEIGHT", defaultsTo: "600");
+  parser.addOption("assetsPath", abbr: "ap", help: "The absolute path to the asset directory "
+      "built by runing flutter build asset");
+  // TODO not used right now
+  parser.addOption("dpi", abbr: "dpi", help: "The DPI of the app, if not set this is calculated NOT USED RIGHT NOW",
+    valueHelp: "DPI");
+
+  ArgResults results = parser.parse(arguments);
+  List<String> restArgs = results.rest;
+
+  String stringWidth = results["width"];
+  String stringHeight = results["height"];
+
+  int width = int.tryParse(stringWidth);
+  int height = int.tryParse(stringHeight);
+
+  if(width == null) {
+    print('Width "$stringWidth" is not an integer');
     return;
   }
-  List<String> cmdArgs = arguments.sublist(1);
+  if(height == null) {
+    print('Height "$stringHeight" is not an integer');
+    return;
+  }
 
+  String assetPath = results["assetsPath"];
+
+  print("Started with args: $arguments");
 
   if (glfw.init()) {
     final window = DesktopWindow.createSnapshotMode(
-      width: 375, height: 625, title: "Flutter Demo",
+      width: width, height: height, title: "Flutter Demo",
       assetPath: assetPath, icuDataPath: "icudtl.dat",
-      commandLineArgs: cmdArgs,
+      commandLineArgs: restArgs,
     //  commandLineArgs: [ "app", "--dart-non-checked-mode" ],
     );
     window.run();
