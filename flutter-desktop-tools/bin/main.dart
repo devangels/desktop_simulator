68e6d167f3605d89212888c626798b0f970c4746
Norbert <norbertkozsir@gmail.com>
2018-12-15 15:29:48 +0100

    added tooling project for the flutter simulators

---
+++ flutter-desktop-tools/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_desktop_tools/flutter_desktop_tools.dart' as flutter_desktop_tools;
+import 'package:args/args.dart';
+
+
+main(List<String> arguments) {
+  ArgParser parser = ArgParser();
+}
